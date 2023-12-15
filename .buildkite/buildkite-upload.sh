befe12de6a0890cbada5aabbe47f5003b18efe27
Arnstein Ressem <aressem@yahooinc.com>
2023-12-15 10:57:34 +0100

    Checkout vespa.

--- .buildkite/buildkite-upload.sh
+++ .buildkite/buildkite-upload.sh
@@ -13,7 +13,14 @@ echo "--- :pipeline_upload: uploading pipeline"
 cat <<EOF | buildkite-agent pipeline upload 
 
 steps:
-  - label: ":pipeline: dynamicly generated pipeline"
-    command: find / 
+  - label: ":pipeline: dynamicly generated build"
+    plugins:
+      - hasura/smooth-checkout#v3.1.0:
+          repos:
+            - config:
+              - url: "https://github.com/vespa-engine/vespa.git"
+              - ref: "master"
+              - clone_flags: "--depth 1"
+    command: find $HOME/.buildkite 
 EOF
 
