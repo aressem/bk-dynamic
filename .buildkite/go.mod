73f1ec9588c06a0ceb124590fd8cb88d766752b6
Arnstein Ressem <aressem@yahooinc.com>
2024-02-23 15:41:39 +0100

    Test go pipeline

---
+++ .buildkite/go.mod
@@ -0,0 +1,17 @@
+module pipegen
+
+go 1.20
+
+require (
+    github.com/buildkite/go-pipeline v0.3.2
+    gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+    github.com/buildkite/interpolate v0.0.0-20200526001904-07f35b4ae251 // indirect
+    github.com/google/go-cmp v0.6.0 // indirect
+    github.com/kr/pretty v0.3.1 // indirect
+    github.com/kr/text v0.2.0 // indirect
+    github.com/oleiade/reflections v1.0.1 // indirect
+    github.com/rogpeppe/go-internal v1.9.0 // indirect
+)
---
+++ .buildkite/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "fmt"
+    "github.com/buildkite/go-pipeline"
+    "gopkg.in/yaml.v3"
+)
+
+func main() {
+
+    pipe := &pipeline.Pipeline{}
+    step := &pipeline.CommandStep{
+        Label: "Vespa build!",
+    }
+
+    plug := &pipeline.Plugin{
+        Source: "kubernetes",
+        Config: map[string]any{
+            "podSpec": map[string]any{
+                "containers": []any{
+                    map[string]any{
+                        "args": []string{
+                            "'pwd && git clone --quiet --depth 1 https://github.com/vespa-engine/vespa && export VESPA_VERSION=8.999.1 && (cd vespa && git tag v${VESPA_VERSION}) && make -C vespa -f .copr/Makefile rpms outdir=$(pwd)'",
+                        },
+                        "command": []any{
+                            "sh",
+                            "-c",
+                        },
+                        "image": "docker.io/vespaengine/vespa-build-almalinux-8",
+                        "resources": map[string]any{
+                            "limits": map[string]any{
+                                "cpu":    "7",
+                                "memory": "24G",
+                            },
+                        },
+                    },
+                },
+                "nodeSelector": map[string]any{
+                    "node-arch": "arm64",
+                },
+                "priorityClassName": "system-node-critical",
+            },
+        },
+    }
+
+    step.Plugins = append(step.Plugins, plug)
+    pipe.Steps = append(pipe.Steps, step)
+
+    yout, err := yaml.Marshal(pipe)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(string(yout))
+}
+
