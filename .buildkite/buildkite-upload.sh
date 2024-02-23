83b4bacb171ef95090aa0c0eaf6a59aa309bd657
Arnstein Ressem <aressem@yahooinc.com>
2024-02-23 11:35:40 +0100

    Try arm64 again

--- .buildkite/buildkite-upload.sh
+++ .buildkite/buildkite-upload.sh
@@ -19,18 +19,18 @@ steps:
   plugins:
   - kubernetes:
       podSpec:
-#        nodeSelector:
-#          node-arch: arm64
+        nodeSelector:
+          node-arch: arm64
         priorityClassName: system-node-critical
         containers:
         - image: docker.io/vespaengine/vespa-build-almalinux-8
           command: [sh, -c]
           args:
           - "'pwd && git clone --quiet --depth 1 https://github.com/vespa-engine/vespa && export VESPA_VERSION=8.999.1 && (cd vespa && git tag v${VESPA_VERSION}) && make -C vespa -f .copr/Makefile rpms outdir=$(pwd)'"
-#          resources:
-#            limits:
-#              cpu: "7"
-#              memory: "30G"
+          resources:
+            limits:
+              cpu: "7"
+              memory: "24G"
 EOF
 
 #steps:
